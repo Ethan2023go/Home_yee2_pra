8bd8e75b9a69587ec497ac7086e106bd83aa3779
Ethan <easonbest123@gmail.com>
2024-02-20 16:29:35 +0800

    新增時間瀏覽功能

---
+++ api/db.php
@@ -0,0 +1,171 @@
+<?php 
+date_default_timezone_set("Asia/Taipei");
+session_start();
+class DB{
+
+    protected $dsn = "mysql:host=localhost;charset=utf8;dbname=db16";
+    protected $pdo;
+    protected $table;
+    
+    public function __construct($table)
+    {
+        $this->table=$table;
+        //$this->pdo=new PDO($this->dsn,'s1120401','s1120401');
+        $this->pdo=new PDO($this->dsn,'root','');
+    }
+
+
+    function all( $where = '', $other = '')
+    {
+        $sql = "select * from `$this->table` ";
+        $sql =$this->sql_all($sql,$where,$other);
+        return  $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    function count( $where = '', $other = ''){
+        $sql = "select count(*) from `$this->table` ";
+        $sql=$this->sql_all($sql,$where,$other);
+        return  $this->pdo->query($sql)->fetchColumn(); 
+    }
+    private function math($math,$col,$array='',$other=''){
+        $sql="select $math(`$col`)  from `$this->table` ";
+        $sql=$this->sql_all($sql,$array,$other);
+        return $this->pdo->query($sql)->fetchColumn();
+    }
+    function sum($col='', $where = '', $other = ''){
+        return  $this->math('sum',$col,$where,$other);
+    }
+    function max($col, $where = '', $other = ''){
+        return  $this->math('max',$col,$where,$other);
+    }  
+    function min($col, $where = '', $other = ''){
+        return  $this->math('min',$col,$where,$other);
+    }  
+    
+    function find($id)
+    {
+        $sql = "select * from `$this->table` ";
+    
+        if (is_array($id)) {
+            $tmp = $this->a2s($id);
+            $sql .= " where " . join(" && ", $tmp);
+        } else if (is_numeric($id)) {
+            $sql .= " where `id`='$id'";
+        } 
+        //echo 'find=>'.$sql;
+        $row = $this->pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
+        return $row;
+    }
+    
+    function save($array){
+        if(isset($array['id'])){
+            $sql = "update `$this->table` set ";
+    
+            if (!empty($array)) {
+                $tmp = $this->a2s($array);
+            } 
+        
+            $sql .= join(",", $tmp);
+            $sql .= " where `id`='{$array['id']}'";
+        }else{
+            $sql = "insert into `$this->table` ";
+            $cols = "(`" . join("`,`", array_keys($array)) . "`)";
+            $vals = "('" . join("','", $array) . "')";
+        
+            $sql = $sql . $cols . " values " . $vals;
+        }
+
+        return $this->pdo->exec($sql);
+    }
+
+    function del($id)
+    {
+        $sql = "delete from `$this->table` where ";
+    
+        if (is_array($id)) {
+            $tmp = $this->a2s($id);
+            $sql .= join(" && ", $tmp);
+        } else if (is_numeric($id)) {
+            $sql .= " `id`='$id'";
+        } 
+        //echo $sql;
+    
+        return $this->pdo->exec($sql);
+    }
+    
+    /**
+     * 可輸入各式SQL語法字串並直接執行
+     */
+    function q($sql){
+        return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+
+    }
+
+    private function a2s($array){
+        foreach ($array as $col => $value) {
+            $tmp[] = "`$col`='$value'";
+        }
+        return $tmp;
+    }
+
+    private function sql_all($sql,$array,$other){
+
+        if (isset($this->table) && !empty($this->table)) {
+    
+            if (is_array($array)) {
+    
+                if (!empty($array)) {
+                    $tmp = $this->a2s($array);
+                    $sql .= " where " . join(" && ", $tmp);
+                }
+            } else {
+                $sql .= " $array";
+            }
+    
+            $sql .= $other;
+            // echo 'all=>'.$sql;
+            // $rows = $this->pdo->query($sql)->fetchColumn();
+            return $sql;
+        } 
+    }
+
+}
+
+function dd($array)
+{
+    echo "<pre>";
+    print_r($array);
+    echo "</pre>";
+}
+function to($url){
+    header("location:$url");
+}
+
+
+$Total=new DB('total');
+$User=new DB('user');
+$News=new DB('news');
+$Que=new DB('que');
+$Log=new DB('log');
+
+
+// 每日網站瀏覽人數累積 function
+if(!isset($_SESSION['visited'])){
+// 判斷Session 有沒有瀏覽過
+    if($Total->count(['date'=>date('Y-m-d')])>0){
+        // 判斷今天日期(資料)存不存在
+        // 資料庫中有沒有一筆以上的資料，如果有(>0)那今天就是存在的
+        $total=$Total->find(['date'=>date('Y-m-d')]);
+        // 找到今天的日期
+        $total['total']++;
+        // 人數+1
+        $Total->save($total);
+    }else{
+        $Total->save(['total'=>1,'date'=>date('Y-m-d')]);
+        // 如果沒有資料，就新增一筆人數為1開始的今日資料
+    }
+    $_SESSION['visited']=1;
+}
+
+
+?>
